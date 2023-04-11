d48c73df1178e008830b44eedf9b8f68947b56e5
Baryshewww <baryshev-tm@mail.ru>
2023-04-11 21:54:32 +0300

    Установит и настроит Babel

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,13 @@ module.exports = {
       patterns: [{ from: 'public' }],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node-modules)/,
+        use: ['babel-loader']
+      }
+    ]
+  }
 };
