39bd0546999c04c2d9ab976050a72cc75fa4ad1e
Baryshewww <baryshev-tm@mail.ru>
2023-04-15 20:25:41 +0300

    Разобьёт код на модули

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+import FiltersView from './view/filters-view.js';
+import MenuNavView from './view/menu-nav-view.js';
+import {render} from './render.js';
+
+const siteHeaderFilterElement = document.querySelector('.trip-controls__filters');
+const siteHeaderMenuElement = document.querySelector('.trip-controls__navigation');
+
+render(new MenuNavView(), siteHeaderMenuElement);
+render(new FiltersView(), siteHeaderFilterElement)
 
