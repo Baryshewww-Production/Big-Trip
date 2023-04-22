ac1c4efcd410150819f6bb4d09146aee9d871546
Baryshewww <baryshev-tm@mail.ru>
2023-04-22 15:11:22 +0300

    Делает моковые данные более цельными

--- src/mock/points-mock.js
+++ src/mock/points-mock.js
@@ -1,55 +1,34 @@
 import {getRandomArrayElement, getRandomPositiveInteger} from '../utils.js';
-import {destinationMock} from './destination-mock.js';
+import {createDestinationMock} from './destination-mock.js';
 import {offersMock} from './offers-mock.js';
-import {OFFER_TYPE} from '../const.js';
+import {CITIES, OFFER_TYPE} from '../const.js';
 
 const offerArrayType = Object.entries(OFFER_TYPE);
 
-const pointsMock = [
-  {
+function createPointMock(cities) {
+  const city = getRandomArrayElement(cities);
+  const type = getRandomArrayElement(offerArrayType);
+
+  return {
     basePrice: getRandomPositiveInteger(120, 950),
     dateFrom: '2023-04-18T22:55:56',
     dateTo: '2023-04-19T11:22:11',
-    destination: getRandomArrayElement(destinationMock),
-    id: getRandomPositiveInteger(0,10),
+    destination: createDestinationMock(city),
+    id: getRandomPositiveInteger(0, 10),
     isFavorite: Boolean(getRandomPositiveInteger(0, 1)),
     offers: getRandomArrayElement(offersMock),
-    type: getRandomArrayElement(offerArrayType),
-  },
-  {
-    basePrice: getRandomPositiveInteger(205, 800),
-    dateFrom: '2023-04-24T22:55:56',
-    dateTo: '2023-04-25T11:22:11',
-    destination: getRandomArrayElement(destinationMock),
-    id: getRandomPositiveInteger(0,10),
-    isFavorite: Boolean(getRandomPositiveInteger(0, 1)),
-    offers: getRandomArrayElement(offersMock),
-    type: getRandomArrayElement(offerArrayType),
-  },
-  {
-    basePrice: getRandomPositiveInteger(347, 650),
-    dateFrom: '2023-04-22T22:55:56',
-    dateTo: '2023-04-24T11:22:11',
-    destination: getRandomArrayElement(destinationMock),
-    id: getRandomPositiveInteger(0,10),
-    isFavorite: Boolean(getRandomPositiveInteger(0, 1)),
-    offers: getRandomArrayElement(offersMock),
-    type: getRandomArrayElement(offerArrayType),
-  },
-  {
-    basePrice: getRandomPositiveInteger(360, 450),
-    dateFrom: '2023-04-19T22:55:56',
-    dateTo: '2023-04-21T11:22:11',
-    destination: getRandomArrayElement(destinationMock),
-    id: getRandomPositiveInteger(0,10),
-    isFavorite: Boolean(getRandomPositiveInteger(0, 1)),
-    offers: getRandomArrayElement(offersMock),
-    type: getRandomArrayElement(offerArrayType),
-  },
-];
+    type: type,
+  };
+}
+
+const pointsMock = [];
+
+for (let i = 0; i < 7; i++) {
+  pointsMock.push(createPointMock(CITIES));
+}
 
 function getRandomPoint() {
   return getRandomArrayElement(pointsMock);
 }
 
-export {getRandomPoint};
+export {getRandomPoint, pointsMock};
